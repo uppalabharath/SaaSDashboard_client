846d6cf60e627f262250923a8f755cf3e27c186b
BUPPALA <uppala.bharath@oracle.com>
2017-03-21 00:57:26 +0530

    1. Added index.html 2. Provided backlinks to index,html 3. Rebranded enterprise entity management pages

--- enterprise/js/enterprise.js
+++ enterprise/js/enterprise.js
@@ -1,4 +1,4 @@
-var app = angular.module('EnterpriseApp', ['ngMaterial']);
+var app = angular.module('EnterpriseApp', ['ngMaterial','ngMessages']);
     app.controller('enterpriseCtrl', function($scope,$http,$mdToast){
         $scope.getEnterprises = function() {
             var req = { method: 'GET',
@@ -7,69 +7,110 @@ var app = angular.module('EnterpriseApp', ['ngMaterial']);
             $http(req).then(
                 // Success
                 function(response){
-                    $scope.data = response.data;
+                    var length = response.data.length;
+					if(length == 0){
+						$mdToast.show($mdToast.simple().textContent('No enterprises found').position('top').hideDelay(2000));
+						$scope.data = null;
+					}else {
+						 $scope.data = response.data;
+					}
                 },
                 // Error
                 function(response){
-                    alert('error');
+                	if(response.status === -1){
+						$mdToast.show($mdToast.simple().textContent('Unable to communicate to server').position('top').hideDelay(1000));
+					}else if (response.status === 404) {
+						$mdToast.show($mdToast.simple().textContent('No enterprises found').position('top').hideDelay(2000));
+					}else {
+						$mdToast.show($mdToast.simple().textContent('Internal Server error occurred').position('top').hideDelay(2000));
+					}
                 }
             );
         };
         // Create enterprise
-        $scope.createEnterprise = function() {
-        	var req = {
-        			method: "POST",
-        			url:'http://localhost:8080/SaasDashboard/rest/enterprise/create',
-        			data:$scope.enterprise
-        	};
-        	$http(req).then(
-        			function(response){
-        				//$scope.returnMsg = response.data.message;
-        				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
-        				$scope.enterprise.name='';
-        			},
-        			function(response){
-        				if(response.status === -1){
-        					$mdToast.show($mdToast.simple().textContent('Unable to communicate to server').position('top').hideDelay(1000));
-        				}
-        			}
-        	);
+        $scope.createEnterprise = function(value) {
+        	if(!value){
+        		var req = {
+            			method: "POST",
+            			url:'http://localhost:8080/SaasDashboard/rest/enterprise/create',
+            			data:$scope.enterprise
+            	};
+            	$http(req).then(
+            			function(response){
+            				//$scope.returnMsg = response.data.message;
+            				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
+            				$scope.enterprise=null;
+            				$scope.enterpriseCreateForm.$setPristine();
+            				$scope.enterpriseCreateForm.$setUntouched();
+            			},
+            			function(response){
+            				if(response.status === -1){
+            					$mdToast.show($mdToast.simple().textContent('Unable to communicate to server').position('top').hideDelay(1000));
+            				}else {
+        						$mdToast.show($mdToast.simple().textContent('Internal Server error occurred').position('top').hideDelay(2000));
+        					}
+            			}
+            	);
+        	}else{
+        		$mdToast.show($mdToast.simple().textContent('Some mandatory fields are not filled').position('top').hideDelay(2000));
+        	}
+        	
         	
         };
         // update enterprise
-        $scope.updateEnterprise = function() {
-        	var req = {
-        			method: "POST",
-        			url:'http://localhost:8080/SaasDashboard/rest/enterprise/update',
-        			data:$scope.enterprise
-        	};
-        	$http(req).then(
-        			function(response){
-        				//$scope.returnMsg = response.data.message;
-        				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
-        				$scope.enterprise.name='';
-        				$scope.enterprise.id='';
-        			},
-        			function(response){
-        				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
-        			}
-        	);
+        $scope.updateEnterprise = function(value) {
+        	if(!value){
+        		var req = {
+            			method: "POST",
+            			url:'http://localhost:8080/SaasDashboard/rest/enterprise/update',
+            			data:$scope.enterprise
+            	};
+            	$http(req).then(
+            			function(response){
+            				//$scope.returnMsg = response.data.message;
+            				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
+            				$scope.enterprise = null;
+            				$scope.enterpriseUpdateForm.$setPristine();
+            				$scope.enterpriseUpdateForm.$setUntouched();
+            			},
+            			function(response){
+            				if(response.status === -1){
+        						$mdToast.show($mdToast.simple().textContent('Unable to communicate to server').position('top').hideDelay(1000));
+        					}else {
+        						$mdToast.show($mdToast.simple().textContent('Internal Server error occurred').position('top').hideDelay(2000));
+        					}
+            			}
+            	);
+        	}else{
+        		$mdToast.show($mdToast.simple().textContent('Some mandatory fields are not filled').position('top').hideDelay(2000));
+        	}
+        	
         };
         // Deleting enterprise
-        $scope.deleteEnterprise = function() {
-        	var req = {
-        			method: "GET",
-        			url:'http://localhost:8080/SaasDashboard/rest/enterprise/delete/'+$scope.enterprise.id
-        	};
-        	$http(req).then(
-        			function(response){
-        				//$scope.returnMsg = response.data.message;
-        				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
-        				$scope.enterprise.id='';
-        			},
-        			function(response){
-        				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
-        			}
-        	);
+        $scope.deleteEnterprise = function(value) {
+        	if(!value){
+        		var req = {
+            			method: "GET",
+            			url:'http://localhost:8080/SaasDashboard/rest/enterprise/delete/'+$scope.enterprise.id
+            	};
+            	$http(req).then(
+            			function(response){
+            				$mdToast.show($mdToast.simple().textContent(response.data.message).position('top').hideDelay(1000));
+            				$scope.enterprise=null;
+            				$scope.enterpriseDeleteForm.$setPristine();
+            				$scope.enterpriseDeleteForm.$setUntouched();
+            			},
+            			function(response){
+            				if(response.status === -1){
+        						$mdToast.show($mdToast.simple().textContent('Unable to communicate to server').position('top').hideDelay(1000));
+        					}else {
+        						$mdToast.show($mdToast.simple().textContent('Internal Server error occurred').position('top').hideDelay(2000));
+        					}
+            			}
+            	);
+        	}else{
+        		$mdToast.show($mdToast.simple().textContent('Some mandatory fields are not filled').position('top').hideDelay(2000));
+        	}
+        	
         };
     });
